3fd756aab554a932ca6379366efcfb9707522e25
Ansh87 <87ansh87@gmail.com>
2026-04-30 15:46:24 -0400

    Fix for deployment error

--- floodscore_frontend/nextjs_space/next.config.js
+++ floodscore_frontend/nextjs_space/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   allowedDevOrigins: ['192.168.86.24', '10.250.12.134:8080', 'https://hackathon-project1-production-9a51.up.railway.app'],
   productionBrowserSourceMaps: false,
   turbopack: {},
