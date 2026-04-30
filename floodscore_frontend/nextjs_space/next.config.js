9f960450523614df1274f08ecdb93027395fc6cc
Ansh87 <87ansh87@gmail.com>
2026-04-30 15:18:22 -0400

    Fixed build error

--- floodscore_frontend/nextjs_space/next.config.js
+++ floodscore_frontend/nextjs_space/next.config.js
@@ -2,17 +2,11 @@
 const nextConfig = {
   allowedDevOrigins: ['192.168.86.24', '10.250.12.134:8080', 'https://hackathon-project1-production-9a51.up.railway.app'],
   productionBrowserSourceMaps: false,
+  turbopack: {},
   typescript: {
     ignoreBuildErrors: false,
   },
   images: { unoptimized: true },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.filename = 'static/chunks/[name]-[contenthash:8].js';
-      config.output.chunkFilename = 'static/chunks/[contenthash:16].js';
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
